5d6e3ee95c8db8a4fb94ae99c1f16218546b9ffa
rado-kolev <rado.k.kolev@gmail.com>
2023-10-06 11:27:30 +0100

    Contact form and experience changes

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { client } from '../../client';
 import './Footer.scss';
 
+import emailjs from '@emailjs/browser';
+
 const Footer = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
@@ -27,52 +33,95 @@ const Footer = () => {
       message: formData.message,
     };
 
-    client.create(contact)
+    client
+      .create(contact)
       .then(() => {
+        sendEmail();
         setLoading(false);
         setIsFormSubmitted(true);
       })
       .catch((err) => console.log(err));
   };
 
+  const form = useRef();
+
+  const sendEmail = () => {
+
+    emailjs
+      .sendForm(
+        'service_s7nmzz4',
+        'template_f26uzsf',
+        form.current,
+        'qc5O_DIXTdRG4iL1w'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
-      <h2 className="head-text">Take a coffee & chat with me</h2>
+      <h2 className='head-text'>Take a <span>coffee</span> & <span>chat</span> with me</h2>
 
-      <div className="app__footer-cards">
-        <div className="app__footer-card ">
-          <img src={images.email} alt="email" />
-          <a href="mailto:hello@micael.com" className="p-text">rado.k.kolev@gmail.com</a>
+      {/* <div className='app__footer-cards'>
+        <div className='app__footer-card '>
+          <img src={images.email} alt='email' />
+          <a href='mailto:rado.k.kolev@gmail.com' className='p-text'>
+            rado.k.kolev@gmail.com
+          </a>
         </div>
-        {/* <div className="app__footer-card">
+        <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+44 7746 190904</a>
-        </div> */}
-      </div>
+          <a href="tel:+44 7746 190904" className="p-text">+44 7746 190904</a>
+        </div>
+      </div> */}
       {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
-          <div className="app__flex">
-            <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
-          </div>
-          <div className="app__flex">
-            <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
-          </div>
-          <div>
-            <textarea
-              className="p-text"
-              placeholder="Your Message"
-              value={message}
-              name="message"
+        <form className='app__footer-form app__flex' ref={form} onSubmit={handleSubmit}>
+          <div className='app__flex'>
+            <input
+              className='p-text'
+              type='text'
+              placeholder='Your Name'
+              name='username'
+              value={username}
               onChange={handleChangeInput}
             />
           </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>{!loading ? 'Send Message' : 'Sending...'}</button>
-        </div>
+          <div className='app__flex'>
+            <input
+              className='p-text'
+              type='email'
+              placeholder='Your Email'
+              name='email'
+              value={email}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <textarea
+              className='p-text'
+              placeholder='Your Message'
+              value={message}
+              name='message'
+              onChange={handleChangeInput}
+            />
+          </div>
+          <button
+            type='button'
+            className='p-text'
+            onClick={handleSubmit}
+          >
+            {!loading ? 'Send Message' : 'Sending...'}
+          </button>
+        </form>
       ) : (
         <div>
-          <h3 className="head-text">
-            Thank you for getting in touch!
-          </h3>
+          <h3 className='head-text'>Thank you for getting in touch!</h3>
         </div>
       )}
     </>
@@ -82,5 +131,5 @@ const Footer = () => {
 export default AppWrap(
   MotionWrap(Footer, 'app__footer'),
   'contact',
-  'app__whitebg',
+  'app__whitebg'
 );
--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -25,61 +25,58 @@ const Skills = () => {
 
   return (
     <>
-      <h2 className="head-text">Skills & Experiences</h2>
+      <h2 className='head-text'><span>Skills</span> & <span>Experiences</span></h2>
 
-      <div className="app__skills-container">
-        <motion.div className="app__skills-list">
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'>
           {skills.map((skill) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
-              className="app__skills-item app__flex"
+              className='app__skills-item app__flex'
               key={skill.name}
             >
               <div
-                className="app__flex"
+                className='app__flex'
                 style={{ backgroundColor: skill.bgColor }}
               >
                 <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
-              <p className="p-text">{skill.name}</p>
+              <p className='p-text'>{skill.name}</p>
             </motion.div>
           ))}
         </motion.div>
-        <div className="app__skills-exp">
+        <div className='app__skills-exp'>
           {experiences.map((experience) => (
-            <motion.div
-              className="app__skills-exp-item"
-              key={experience.year}
-            >
-              <div className="app__skills-exp-year">
-                <p className="bold-text">{experience.year}</p>
-              </div>
-              <motion.div className="app__skills-exp-works">
+            <motion.div className='app__skills-exp-item' key={experience.year}>
+              <motion.div className='app__skills-exp-works'>
                 {experience.works.map((work) => (
                   <>
                     <motion.div
                       whileInView={{ opacity: [0, 1] }}
                       transition={{ duration: 0.5 }}
-                      className="app__skills-exp-work"
+                      className='app__skills-exp-work'
                       data-tip
                       data-for={work.name}
                       key={work.name}
                     >
-                      <h4 className="bold-text">{work.name}</h4>
-                      <p className="p-text">{work.company}</p>
+                      <h4 className='bold-text'>{work.name}</h4>
+                      <p className='p-text'>{work.company}</p>
                     </motion.div>
                     <ReactTooltip
                       id={work.name}
-                      effect="solid"
-                      arrowColor="#fff"
-                      className="skills-tooltip"
+                      effect='solid'
+                      arrowColor='#fff'
+                      className='skills-tooltip'
                     >
                       {work.desc}
                     </ReactTooltip>
                   </>
                 ))}
               </motion.div>
+              <div className='app__skills-exp-year'>
+                <p className='bold-text'>{experience.year}</p>
+              </div>
             </motion.div>
           ))}
         </div>
@@ -91,5 +88,5 @@ const Skills = () => {
 export default AppWrap(
   MotionWrap(Skills, 'app__skills'),
   'skills',
-  'app__whitebg',
+  'app__whitebg'
 );
